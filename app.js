fada589cd07d6a5ecaca16a42384898bee4206a1
christianperkins408 <christianperkins408@gmail.com>
2017-04-08 15:33:28 -0400

    final commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var multer = require('multer');
+var upload = multer({dest:'uploads/'});
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res, next){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/upload', upload.single('file'), function(req,res,next){
+  return res.json({size: req.file.size});
+});
+
+app.listen(3000,function(){
+  console.log("Server listening on port 3000")
+});
